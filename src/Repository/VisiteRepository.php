9d81df9b21f9abd774e2d64b1126f96a46179535
Rabarijaona AJ <aj@MacBook-Air-de-Rabarijaona.local>
2026-01-23 00:43:21 +0300

    Phase 11 : ORM (suppression d'un enregistrement)

--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -51,4 +51,14 @@ class VisiteRepository extends ServiceEntityRepository
                 ->getResult();
         }
     }
+    
+    /**
+     * Supprime une visite
+     * @param Visite $visite
+     * @return void
+     */
+    public function remove(Visite $visite): void {
+        $this->getEntityManager()->remove($visite);
+        $this->getEntityManager()->flush();
+    }
 }
