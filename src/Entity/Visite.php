82e5150bc913458adbdc2ee33d11fa191745272a
Rabarijaona AJ <aj@MacBook-Air-de-Rabarijaona.local>
2026-01-26 00:47:16 +0300

    Phase 14: ORM (ajoute table et lien entre tables)

--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -3,8 +3,11 @@
 namespace App\Entity;
 
 use App\Repository\VisiteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Environnement;
 
 #[ORM\Entity(repositoryClass: VisiteRepository::class)]
 class Visite
@@ -34,6 +37,17 @@ class Visite
 
     #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
     private ?\DateTime $datecreation = null;
+
+    /**
+     * @var Collection<int, Environnement>
+     */
+    #[ORM\ManyToMany(targetEntity: Environnement::class)]
+    private Collection $environnements;
+
+    public function __construct()
+    {
+        $this->environnements = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -130,5 +144,29 @@ class Visite
         }else{
             return $this->datecreation->format('d/m/Y');
         }
+    }
+
+    /**
+     * @return Collection<int, Environnement>
+     */
+    public function getEnvironnements(): Collection
+    {
+        return $this->environnements;
+    }
+
+    public function addEnvironnement(Environnement $environnement): static
+    {
+        if (!$this->environnements->contains($environnement)) {
+            $this->environnements->add($environnement);
+        }
+
+        return $this;
+    }
+
+    public function removeEnvironnement(Environnement $environnement): static
+    {
+        $this->environnements->removeElement($environnement);
+
+        return $this;
     }
 }
