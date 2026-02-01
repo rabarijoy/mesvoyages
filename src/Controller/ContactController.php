08cac3cf36d2a1f8916632d42f66c0da70850c78
Rabarijaona AJ <aj@MacBook-Air-de-Rabarijaona.local>
2026-02-01 19:30:14 +0300

    Phase 19 : gestion des mails.

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController {
+    #[Route('/contact', name: 'contact')]
+    public function index(Request $request, MailerInterface $mailer): Response {
+        $contact = new Contact();
+        $formContact = $this->createForm(ContactType::class, $contact);
+        $formContact->handleRequest($request);
+
+        if($formContact->isSubmitted() && $formContact->isValid()){
+            $this->sendEmail($mailer, $contact);
+            $this->addFlash('succes', 'Message envoyé avec succès');
+            return $this->redirectToRoute('contact');
+        }
+
+        return $this->render('pages/contact.html.twig', [
+            'contact' => $contact,
+            'formcontact' => $formContact->createView()
+            ]);
+    }
+
+    public function sendEmail(MailerInterface $mailer, Contact $contact): void
+    {
+        $email = (new Email())
+            ->from($contact->getEmail())
+            ->to('contact@mesvoyages.com')
+            ->subject('Message du site de voyages')
+            ->html(
+                $this->renderView(
+                    'pages/_email.html.twig',
+                    ['contact' => $contact]
+                ),
+                'utf-8'
+            );
+        $mailer->send($email);
+    }
+}
